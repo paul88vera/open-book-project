cb8eb4230e8542fc8951cbeab274b7957eeaeae6
paul88vera <paul88vera@gmail.com>
2022-04-22 15:42:42 -0500

    added quasar css to form

---
+++ src/pages/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-card-section class="login-page">
+    <q-form action="" class="form">
+        <q-input type="text" label="Username" v-model="login.username" />
+        <q-input type="password" label="Password" v-model="login.password" />
+      <q-btn class="btnSubmit full-width" label="Login"></q-btn>
+    </q-form>
+    </q-card-section>
+  </q-page>
+</template>
+
+
+<script>
+// import { defineComponent } from 'vue'
+
+export default {
+  name: 'loginPage',
+  data() {
+    return {
+      login: {
+        username: '',
+        password: ''
+      }
+    }
+  },
+  methods: {
+    submitForm(){
+      if (!this.login.username || !this.login.password) {
+        console.log('error')
+      } else {
+        console.log('login')
+      }
+    }
+  }
+}
+</script>
